d2ab2220aae631eb589f10160ce63cd58a513ecc
Matoz <matozinfo@gmail.com>
2024-03-28 19:59:54 +0800

    chore: test env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,18 @@ import NavigateDemo from './components/navigate';
 import LoginForm from './components/form';
 
 export default async function LoginPage() {
+  const env = {
+    1: process.env.ZEABUR_WEB_URL,
+    2: process.env.ZEABUR_CONSOLE_URL,
+    3: process.env.ZEABUR_web_URL,
+    4: process.env.ZEABUR_console_URL,
+    5: process.env.CONTAINER_HOSTNAME,
+    6: process.env.ZEABUR_GIT_REPO_NAME,
+  };
   return (
     <div className="m-8 flex flex-col gap-8">
+      <Title text={JSON.stringify(env)} />
+
       <Title text="LOGIN PAGE" />
       <NavigateDemo>
         <Title text="NAVIGATE" />
